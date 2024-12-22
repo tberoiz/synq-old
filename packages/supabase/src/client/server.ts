ce2bf03b72d1a3a6077ba0d7f99e2e66a34b5bfb
Telmiyo <iamtelmo@proton.me>
2024-12-22 23:21:24 +0100

    refactor(supabase): enhance transpiling and exports

--- packages/supabase/src/client/server.ts
+++ packages/supabase/src/client/server.ts
@@ -1,6 +1,5 @@
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
-import { type Provider } from "@supabase/supabase-js";
 
 export async function createClient() {
   const cookieStore = await cookies();
