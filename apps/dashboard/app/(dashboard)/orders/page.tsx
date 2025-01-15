b56778028d8a065ea5b45056d8d16a129e5810a5
Telmiyo <iamtelmo@proton.me>
2025-01-15 20:30:15 +0100

    chore: update content layout

--- apps/dashboard/app/(dashboard)/orders/page.tsx
+++ apps/dashboard/app/(dashboard)/orders/page.tsx
@@ -1,9 +1,5 @@
 import { OrdersTable } from "@ui/tables/orders-table";
 
 export default function OrdersPage() {
-  return (
-    <div className="flex flex-1 flex-col gap-4 p-4">
-      <OrdersTable />
-    </div>
-  );
+  return <OrdersTable />;
 }
--- apps/dashboard/ui/layouts/content/app-content.tsx
+++ apps/dashboard/ui/layouts/content/app-content.tsx
@@ -1,3 +1,11 @@
-export default function AppContent({ children }: { children: React.ReactNode }) {
-  return <div className="flex flex-1 flex-col gap-4 p-4 z-10">{children}</div>;
-};
+export default function AppContent({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex flex-1 flex-col gap-4 p-4 w-full">
+        <div className="flex flex-1 flex-col gap-4 p-4 z-10">{children}</div>
+    </div>
+  );
+}
