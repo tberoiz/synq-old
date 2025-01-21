dad145d572c5f1d21fc3c07f4937ca51c0fdfce5
Telmiyo <iamtelmo@proton.me>
2025-01-21 20:55:00 +0100

    refactor: udpate overview layout

--- apps/dashboard/app/(dashboard)/overview/page.tsx
+++ apps/dashboard/app/(dashboard)/overview/page.tsx
@@ -3,7 +3,6 @@ import { SellingChannelsChart } from "@ui/charts/selling-channels-chart";
 import { InventoryPerformanceChart } from "@ui/charts/inventory-performance-chart";
 import { OverviewChart } from "@ui/charts/total-revenue-chart";
 import { TopItemsChart } from "@ui/charts/top-items-chart";
-import { Gauge } from "lucide-react";
 import { StockChart } from "@ui/charts/stock-chart";
 
 export default function OverviewPage() {
@@ -12,20 +11,12 @@ export default function OverviewPage() {
       <div className="grid gap-4 md:grid-cols-1 lg:grid-cols-1">
         <OverviewChart />
       </div>
-      <Card>
-        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-4">
-          <CardTitle className="text-lg font-medium">Performance</CardTitle>
-          <Gauge size={16} />
-        </CardHeader>
-        <CardContent>
-          <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-            <TopItemsChart />
-            <InventoryPerformanceChart />
-            <SellingChannelsChart />
-            <StockChart />
-          </div>
-        </CardContent>
-      </Card>
+      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+        <TopItemsChart />
+        <InventoryPerformanceChart />
+        <SellingChannelsChart />
+        <StockChart />
+      </div>
     </>
   );
 }
