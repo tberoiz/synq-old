92e31b75676b3a466fa8109f4f72f645b19610d7
Telmiyo <iamtelmo@proton.me>
2025-02-19 11:52:33 +0100

    refactor: update sidebar layout

--- apps/web/app/(dashboard)/layout.tsx
+++ apps/web/app/(dashboard)/layout.tsx
@@ -2,13 +2,14 @@ import { AppSidebar } from "@ui/layouts/sidebar/app-sidebar";
 import { SidebarInset, SidebarProvider } from "@synq/ui/sidebar";
 import AppHeader from "@ui/layouts/header/app-header";
 import AppContent from "@ui/layouts/content/app-content";
+import * as React from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider
       style={
         {
-          "--sidebar-width": "4rem",
+          "--sidebar-width": "12rem",
         } as React.CSSProperties
       }
     >
--- apps/web/ui/layouts/header/app-header.tsx
+++ apps/web/ui/layouts/header/app-header.tsx
@@ -1,27 +1,15 @@
 import React, { memo } from "react";
 import NavUser from "./nav-user";
-import { Button } from "@synq/ui/button";
 import { SidebarFooter, SidebarTrigger } from "@synq/ui/sidebar";
-import { Bell } from "lucide-react";
 
 const AppHeader: React.FC = () => {
   return (
-    <header className="sticky top-0 z-50 flex shrink-0 items-center justify-between gap-2 border-b bg-background p-2">
-      <div></div>
+    <header className="sticky top-0 z-50 flex shrink-0 items-center justify-between gap-2 border-b bg-background p-4">
+      <div>
+        <SidebarTrigger className="ml-1" />
+      </div>
       <SidebarFooter>
         <div className="flex gap-2 items-center">
-          {/* <Button
-            size="icon"
-            variant="outline"
-            className="rounded-full h-8 w-8 px-4 relative"
-          >
-            <Bell className="size-4" />
-            <div
-              className="absolute right-0 top-0 w-2 h-2 rounded-full"
-              style={{ backgroundColor: "rgb(255, 208, 43)" }}
-            />
-          </Button> */}
-
           <NavUser />
         </div>
       </SidebarFooter>
