3020fdf191de3ec894aebad4124602511f21371a
Telmiyo <iamtelmo@proton.me>
2025-03-01 22:01:49 +0100

    chore: add supabase host on next config to support image domain

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@synq/ui"],
+  images: {
+    domains: [
+      "127.0.0.1", // For local development
+      "your-supabase-project-ref.supabase.co", // TODO: Add for production
+    ],
+  },
 };
 
 export default nextConfig;
