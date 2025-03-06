df46023b427dbf1da2b0e5c26b03d94b0d96c01b
Telmiyo <iamtelmo@proton.me>
2025-03-06 16:36:12 +0100

    chore: reset inventory page

--- apps/web/app/(dashboard)/inventory/page.tsx
+++ apps/web/app/(dashboard)/inventory/page.tsx
@@ -1,41 +1,11 @@
-"use client";
+import { Suspense } from "react";
 
-import React, { lazy, Suspense } from "react";
-import { useQuery } from "@tanstack/react-query";
-
-import { List, Package, Plus } from "lucide-react";
-
-import { Button } from "@synq/ui/button";
 import { Skeleton } from "@synq/ui/skeleton";
-
-import { CreateItemDialog } from "@ui/dialogs/inventory/create-item-dialog";
-
-import {
-  fetchAllItems,
-  fetchInventoryBatches,
-} from "@synq/supabase/queries/inventory";
-import { DialogTrigger } from "@synq/ui/dialog";
 import { Tabs, TabsList, TabsTrigger, TabsContent } from "@synq/ui/tabs";
-import { AddNewBatchDialog } from "@ui/dialogs/inventory/add-new-batch-dialog";
 
-const BatchesDataTable = lazy(
-  () => import("@ui/data-tables/inventory/batches-data-table"),
-);
-const ItemsDataTable = lazy(
-  () => import("@ui/data-tables/inventory/items-data-table"),
-);
+import { List, Package } from "lucide-react";
 
-const InventoryPage = () => {
-  const { data: batches, isLoading: isFetchingBatches } = useQuery({
-    queryKey: ["user_inventory_batches"],
-    queryFn: fetchInventoryBatches,
-  });
-
-  const { data: items, isLoading: isFetchingItems } = useQuery({
-    queryKey: ["user_inv_items"],
-    queryFn: fetchAllItems,
-  });
-
+export default async function InventoryPage() {
   return (
     <Tabs defaultValue="items" className="space-y-4">
       <div className="flex items-center justify-between">
@@ -60,13 +30,7 @@ const InventoryPage = () => {
               <Skeleton className="h-[300px] w-full" />
             </div>
           }
-        >
-          <ItemsDataTable
-            data={items || []}
-            loading={isFetchingItems}
-            actions={<CreateItemDialog />}
-          />
-        </Suspense>
+        ></Suspense>
       </TabsContent>
 
       {/* Batches Tab */}
@@ -78,27 +42,8 @@ const InventoryPage = () => {
               <Skeleton className="h-[300px] w-full" />
             </div>
           }
-        >
-          <BatchesDataTable
-            data={batches || []}
-            loading={isFetchingBatches}
-            actions={
-              <AddNewBatchDialog
-                trigger={
-                  <DialogTrigger asChild>
-                    <Button variant="outline" size="sm">
-                      <Plus className="h-4 w-4" />
-                      New
-                    </Button>
-                  </DialogTrigger>
-                }
-              ></AddNewBatchDialog>
-            }
-          />
-        </Suspense>
+        ></Suspense>
       </TabsContent>
     </Tabs>
   );
-};
-
-export default InventoryPage;
+}
