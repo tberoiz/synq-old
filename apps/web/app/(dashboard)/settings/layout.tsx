e41c647aee9044356c2df1dd3a8c8984fcfc39ae
Telmiyo <iamtelmo@proton.me>
2025-03-06 15:08:33 +0100

    chore: update settings layout

--- apps/web/app/(dashboard)/settings/layout.tsx
+++ apps/web/app/(dashboard)/settings/layout.tsx
@@ -1,18 +1,11 @@
-import { CardLayout } from "@ui/layouts/content/card-layout";
-import { Settings } from "lucide-react";
-
 export default function SettingsLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <CardLayout
-      title="Settings"
-      icon={<Settings strokeWidth={1} />}
-      description="Manage your account settings and preferences."
-    >
+    <>
       {children}
-    </CardLayout>
+    </>
   );
 }
